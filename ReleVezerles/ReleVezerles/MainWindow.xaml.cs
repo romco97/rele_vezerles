718529e7ff8d354b5dec4ae1f1e8bb6da308210f
EccioHD <69851981+EccioHD@users.noreply.github.com>
2020-08-18 15:56:38 +0200

    Update MainWindow.xaml.cs

--- ReleVezerles/ReleVezerles/MainWindow.xaml.cs
+++ ReleVezerles/ReleVezerles/MainWindow.xaml.cs
@@ -25,8 +25,32 @@ namespace ReleVezerles
         public static int elektrodakszama;
         public static string merestipus;
 
+        static void wennerafla()
+        {
+            /* MINTA
+              
+               ABCD
+               A-B-C-D
+               A--B--C--D   */
+
+            /* TERVEZÉS
+               0. (előfeltétel) hány szonda van? (gombon lehet majd állitani ---> változó(szondadb))
+               1. (algoritmus) ABCD funkcionalitású szondák, addig mér amíg az algoritmus alapján a D nem lesz nagyobb a szondadb változónknál
+               2. (ciklus) amint nagyobb lenne a D szonda sorszáma mint a szondadb, akkor egy (helykoz) valtozó 1-el nől, és úgy fut le nullától
+               3. (lezárás) a mérés addig fut amíg képes rá, ez alatt azt értjük,
+               hogy ha az A szonda első helyen áll, és a helyköz úgymond eléri azt a maximális számot,
+               hogy a D szonda már nem tudna úgy elhelyezkedni hogy a sorszáma ne legyen nagyobb a szonda db-nál,
+               ha erre már nem képes akkor már nem mérünk tovább     */
+
+            //MEGVALÓSÍTÁS
+
+
+        }
+
+
         public MainWindow()
         {
+
             InitializeComponent();
         }
     }
